888a856fa5db9390ab00ab11e72e186b34d90341
leventekoszo <leventekoszo33@gmail.com>
2025-10-15 15:15:42 +0200

    inputs created

---
+++ edzestervezo_es_kaloriakalkulator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edzestervezo_es_kaloriakalkulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Név: ");
+            string[] name = Console.ReadLine().Split(' ');
+            Console.Write("Testsúly: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Edzés célja: ");
+            int goal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1. Állóképesség");
+            Console.WriteLine("2. Izomtömeg növelés");
+            Console.WriteLine("3. Fogyás");
+        }
+    }
+}
