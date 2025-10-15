228fb60fb47a6429d0e8887a05600741be574662
leventekoszo <leventekoszo33@gmail.com>
2025-10-15 15:28:00 +0200

    calculations created

--- edzestervezo_es_kaloriakalkulator/Program.cs
+++ edzestervezo_es_kaloriakalkulator/Program.cs
@@ -12,6 +12,8 @@ namespace edzestervezo_es_kaloriakalkulator
         {
             Console.Write("Név: ");
             string[] name = Console.ReadLine().Split(' ');
+            string lastName = name[0];
+            string firstName = name[1];
             Console.Write("Testsúly: ");
             double weight = Convert.ToDouble(Console.ReadLine());
             Console.Write("Edzés célja: ");
@@ -19,6 +21,54 @@ namespace edzestervezo_es_kaloriakalkulator
             Console.WriteLine("1. Állóképesség");
             Console.WriteLine("2. Izomtömeg növelés");
             Console.WriteLine("3. Fogyás");
+
+            string workoutType = string.Empty;
+            int workoutDuration = 0;
+            double calorieMultiplier = 0;
+
+            if (goal == 1)
+            {
+                workoutType = "Állóképesség";
+                workoutDuration = 45;
+                calorieMultiplier = 0.12;
+            }
+            else if (goal == 2)
+            {
+                workoutType = "Izomtömeg növelés";
+                workoutDuration = 60;
+                calorieMultiplier = 0.1;
+            }
+            else if (goal == 3)
+            {
+                workoutType = "Fogyás";
+                workoutDuration = 30;
+                calorieMultiplier = 0.15;
+            }
+            else
+            {
+                workoutType = "Érvénytelen cél";
+                workoutDuration = 0;
+            }
+
+            Console.Write("Hány napot szeretnél edzeni?: ");
+            int workoutDays = Convert.ToInt32(Console.ReadLine());
+
+            int strengthlevel = 1;
+
+            double totalWorkoutTime = 0;
+
+            for (int i = 1; i <= workoutDays; i++)
+            {
+                Console.Write($"{i}. edzésnap erősségi szintje (1-5): ");
+                while (strengthlevel < 1 || strengthlevel > 5)
+                {
+                    strengthlevel = Convert.ToInt32(Console.ReadLine());
+                }
+                double dailyWorkoutTime = workoutDuration * (1 + 0.1 * strengthlevel);
+                totalWorkoutTime += dailyWorkoutTime;
+            }
+
+            double totalCaloriesBurned = weight * totalWorkoutTime * calorieMultiplier;
         }
     }
 }
